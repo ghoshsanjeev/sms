fe26ecc81506401ca863c998778dd0c253a022eb
ksalvi <kunal.salvi@hds.com>
2015-11-03 20:05:14 +0530

    admin page

--- src/main/java/com/batb/sms/constant/ViewNames.java
+++ src/main/java/com/batb/sms/constant/ViewNames.java
@@ -6,4 +6,5 @@ public interface ViewNames {
 	String LOGIN="login";
 	String STUDENT_ATTENDANCE="student_attendance";
 	String MARK_SHEET_ENTRY="mark_sheet_entry_format";
+	String ADMIN_DOCUMENT_ENTRY="admin_document_entry";
 }
---
+++ src/main/java/com/batb/sms/controller/AdminController.java
@@ -0,0 +1,17 @@
+package com.batb.sms.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.batb.sms.constant.ViewNames;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+	
+	@RequestMapping(value = "/documentEntry")
+	public String getDocumentEntryPage() {
+		return ViewNames.ADMIN_DOCUMENT_ENTRY;
+	}
+	
+}
